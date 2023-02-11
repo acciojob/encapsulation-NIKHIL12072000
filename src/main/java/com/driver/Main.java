157bac3e302b85ba56f4b5ad7c085a2bc11a8c67
Nikhil <dachepallinikhil@gmail.com>
2023-02-11 20:36:38 +0530

    Tasks Completed

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,11 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String args[]){
+        RWOnly obj=new RWOnly();
+        //obj.name;
+        // System.out.println(obj.name);
+        obj.setName("Nikhil");
+        obj.getName();
+    }
 }
